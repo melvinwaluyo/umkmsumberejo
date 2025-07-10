9c70e19024146b600318387da4d49cda0fc0c15a
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2025-07-10 23:00:56 +0700

    fix: Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -56,7 +56,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
           {/* Logo dan Judul Website */}
           <div className="flex items-center space-x-4">
-            <Image src="/logo.png" alt="Logo" width={40} height={40} className="rounded-full" />
+            <Image src="/Logo.png" alt="Logo" width={40} height={40} className="rounded-full" />
             <Link href="/" className="text-xl font-bold text-green-700">
               UMKM Sumberejo
             </Link>
